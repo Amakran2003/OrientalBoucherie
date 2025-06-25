bc29c5c00af5c38dd0b35b7d3bad1b9e289663d3
Abderrazaq MAKRAN <145991267+Amakran2003@users.noreply.github.com>
2025-06-25 11:22:37 +0200

    Updates

--- assets/ContactPage.BqUx2IOo.js
+++ assets/ContactPage.BqUx2IOo.js
@@ -1,4 +1,4 @@
-import{h as c,j as e,M as p,P as g,k as y,r as i,B as b,C as f,H as j,s as v}from"./index.BcsZm8ZK.js";import{P as k}from"./PageHeader.DDR5ggh0.js";/**
+import{h as c,j as e,M as p,P as g,k as y,r as i,B as b,C as f,H as j,s as v}from"./index.DO26QJvl.js";import{P as k}from"./PageHeader.CghdUQg9.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/sliders-horizontal.DmV-ZYWY.js
+++ assets/sliders-horizontal.DmV-ZYWY.js
@@ -1,4 +1,4 @@
-import{h as e}from"./index.BcsZm8ZK.js";/**
+import{h as e}from"./index.DO26QJvl.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
